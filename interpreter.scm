dea99443956516797e4d21ede6cfdf80da3e7616
Tom Dooner <tom@causes.com>
2013-02-23 23:19:46 -0500

    Update environment binding after interpreting an expression
    A call to interpret-stmt needs an environment list (something like:
      '((a 4) (b 5))
    ) that is updated with the correct state up until that point. This adds
    a let variable "env" which is such a list. It is "set!", after each
    statement executes, to the return value from interpret-stmt. It seems
    like this usage of the variable is in accordance with the "General
    Guidelines" on the assignment.
    
    One potential TODO: I think it might actually be more appropriate to do
    the "set!" in the interpret-assign function, as that is one of two
    places that the environment can change. (It, and the to-be-written
    interpret-declare for "var x;" are the only two that I can think of)
    
    basic.c is a test file with stuff that we have implemented in the
    interpreter.

--- interpreter.scm
+++ interpreter.scm
@@ -2,35 +2,51 @@
 
 (define interpret
   (lambda (filename)
-    (let ([environment '()])
-      (interpret-statement-list (parse filename) environment))))
+    (let ((env '()))
+      (interpret-statement-list (parser filename) env))))
 
 (define interpret-statement-list
-  (lambda (parsetree environment)
-    ((null? parsetree) environment)
-    (else
-      (interpret-stmt (car parsetree) environment))))
+  (lambda (parsetree env)
+    (cond
+      ((null? parsetree) env)
+      (else
+        (begin
+          (set! env (interpret-stmt (car parsetree) env))
+          (interpret-statement-list (cdr parsetree) env))
+        ))))
 
 (define interpret-stmt
-  (lambda (stmt environment)
+  (lambda (stmt env)
     (cond
-      ((eq? (car stmt) '=) (interpret-assign (stmt environment)))
+      ((eq? (car stmt) '=) (interpret-assign stmt env))
       ; and so forth, for all the stuff we need...
-      ;((eq? (car stmt) '+) (interpret-assign (stmt environment)))
-      ;((eq? (car stmt) '-) (interpret-assign (stmt environment)))
-      ;((eq? (car stmt) '*) (interpret-assign (stmt environment)))
-      ;((eq? (car stmt) '/) (interpret-assign (stmt environment)))
-      ;((eq? (car stmt) '%) (interpret-assign (stmt environment)))
-      ;((eq? (car stmt) '-) (interpret-assign (stmt environment)))  ;unary -
+      ;((eq? (car stmt) '+) (interpret-assign (stmt env)))
+      ;((eq? (car stmt) '-) (interpret-assign (stmt env)))
+      ;((eq? (car stmt) '*) (interpret-assign (stmt env)))
+      ;((eq? (car stmt) '/) (interpret-assign (stmt env)))
+      ;((eq? (car stmt) '%) (interpret-assign (stmt env)))
+      ;((eq? (car stmt) '-) (interpret-assign (stmt env)))  ;unary -
       ;
-      ;((eq? (car stmt) '<) (interpret-assign (stmt environment)))
-      ;((eq? (car stmt) '>) (interpret-assign (stmt environment)))
-      ;((eq? (car stmt) '>=) (interpret-assign (stmt environment)))
-      ;((eq? (car stmt) '<=) (interpret-assign (stmt environment)))
-      ;((eq? (car stmt) '!=) (interpret-assign (stmt environment)))
-      ;((eq? (car stmt) '==) (interpret-assign (stmt environment)))
+      ;((eq? (car stmt) '<) (interpret-assign (stmt env)))
+      ;((eq? (car stmt) '>) (interpret-assign (stmt env)))
+      ;((eq? (car stmt) '>=) (interpret-assign (stmt env)))
+      ;((eq? (car stmt) '<=) (interpret-assign (stmt env)))
+      ;((eq? (car stmt) '!=) (interpret-assign (stmt env)))
+      ;((eq? (car stmt) '==) (interpret-assign (stmt env)))
       ;
-      ;((eq? (car stmt) '&&) (interpret-assign (stmt environment)))
-      ;((eq? (car stmt) '||) (interpret-assign (stmt environment)))
-      ;((eq? (car stmt) '!) (interpret-assign (stmt environment)))  ;unary !
+      ;((eq? (car stmt) '&&) (interpret-assign (stmt env)))
+      ;((eq? (car stmt) '||) (interpret-assign (stmt env)))
+      ;((eq? (car stmt) '!) (interpret-assign (stmt env)))  ;unary !
 )))
+
+(define interpret-assign
+  (lambda (stmt env)
+    (update-environment (cadr stmt) (caddr stmt) env)))
+
+(define update-environment
+  (lambda (binding value env)
+    (cond
+      ((null? env) (cons (cons binding (cons value '())) '()))
+      ((eq? (car (car env)) binding) (cons (cons binding (cons value '())) (cdr env)))
+      (else (cons (car env) (update-environment binding value (cdr env))))
+      )))
