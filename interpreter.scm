0b06d7afbfb8937cc3b0739d05cf3fe074507d96
Brian Stack <bis12@case.edu>
2013-02-24 22:20:02 -0500

    Add boolean operators

--- interpreter.scm
+++ interpreter.scm
@@ -68,6 +68,9 @@
       ((eq? (car stmt) '>) ((interpret-binary >) stmt env))
       ((eq? (car stmt) '<=) ((interpret-binary <=) stmt env))
       ((eq? (car stmt) '>=) ((interpret-binary >=) stmt env))
+      ((eq? (car stmt) '&&) ((interpret-binary (lambda (x y) (and x y))) stmt env))
+      ((eq? (car stmt) '||) ((interpret-binary (lambda (x y) (or x y))) stmt env))
+      ((eq? (car stmt) '!) (interpret-not stmt env))
 )))
 
 (define interpret-declare
@@ -96,6 +99,10 @@
     (cond
       ((eq? (length stmt) 3) ((interpret-binary -) stmt env))
       (else (- 0 (interpret-stmt-value (cadr stmt) env))))))
+
+(define interpret-not
+  (lambda (stmt env)
+    (not (interpret-stmt-value (cadr stmt) env))))
 
 ; declares a variable and adds it to the environment with the value 'None
 ; for "var x;"
