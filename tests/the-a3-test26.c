a80c156bd1d4196ed7784bd26336635f2c9d3b13
Tom Dooner <tom@causes.com>
2013-04-29 19:26:07 -0400

    Fix the straggler test

--- tests/the-a3-test26.c
+++ tests/the-a3-test26.c
@@ -1,20 +1,20 @@
 description: The straggler test
 value: 115
 
-var a = 10;
-var b = 20;
+static var a = 10;
+static var b = 20;
 
-bmethod() {
+static bmethod() {
   var b = 30;
   return a + b;
 }
 
-cmethod() {
+static cmethod() {
   var a = 40;
   return bmethod() + a + b;
 }
 
-main () {
+static main () {
   var b = 5;
   return cmethod() + a + b;
 }
